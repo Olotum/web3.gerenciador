12e350ad77758b78d8816f13ff780f6f995cf766
Olotum <joose.gabriiel@gmail.com>
2023-05-12 13:44:32 -0300

    Project Clean

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import logo from '../../assets/img/logo.svg'
+
+import './styles.css'
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="DSMeta"/>
+                <h1>DSMeta</h1>
+                <p>
+                    Desenvolvido por 
+                    <a href="https://www.linkedin.com/in/jos%C3%A9-gabriel-ferreira-dos-santos-621a23226/"> José Gabriel F. Dos Santos </a>
+                </p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
